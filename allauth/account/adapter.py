2fe42f718ab94b4cd7d82091c27b34b3d3d8b352
joebos <frank_lqx@yahoo.com>
2014-07-09 21:03:12 -0400

    ...

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -238,7 +238,6 @@ class DefaultAccountAdapter(object):
                 if message:
                     messages.add_message(request, level, message,
                                          extra_tags=extra_tags)
-                    i = 0
             except TemplateDoesNotExist:
                 pass
 
